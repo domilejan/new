00b3e469395423b5878450074a8a19fffa9b5b6d
Aziz <aziz@humanitec.com>
2019-02-25 14:12:41 +0100

    remove wrong input

--- src/lib/client.module.ts
+++ src/lib/client.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ClientComponent } from './client.component';
 import { MidgardSharedTranslationModule } from '@libs/midgard-angular/src/lib/modules/translation/translation.shared.module';
-import { ContactDetailComponent } from './pages/contact-detail/contact-detail.component';
 import { ClientRoutingModule } from './client-routing.module';
 
 @NgModule({
