34bc3c824f301709dae6567f1d1a17be5e9b22a9
Aziz <aziz@humanitec.com>
2019-03-18 11:29:09 +0100

    refactor epics

--- src/lib/state/client.epics.ts
+++ src/lib/state/client.epics.ts
@@ -15,105 +15,111 @@ import {
   CREATE_CLIENT, DELETE_CLIENT, LOAD_ALL_CLIENTS, LOAD_ONE_CLIENT, loadOneClientCommit,
   UPDATE_CLIENT
 } from './client.actions';
+import { Injectable } from '@angular/core';
 
-const httpService = new HttpService();
+@Injectable()
+export class ClientsEpics {
+  /**
+   * this is here to handle asynchronous actions and will be triggered when LOAD_DATA_CLIENTS action is dispatched
+   * @param {Observable} action$ - the current action
+   */
+  loadAllClientsEpic = action$ => {
+    return action$.pipe(
+      ofType(LOAD_ALL_CLIENTS),
+      switchMap((action: any) => {
+        return this.httpService.makeRequest('get', `${environment.API_URL}/client`, {}, true).pipe(
+          // If successful, dispatch success action with result
+          map(res => loadClientsCommit(res.data)),
+          // If request fails, dispatch failed action
+          catchError((error) => of(loadClientsFail(error)))
+        );
+      })
+    );
+  }
 
-/**
- * this is here to handle asynchronous actions and will be triggered when LOAD_DATA_CLIENTS action is dispatched
- * @param {Observable} action$ - the current action
- */
-const loadAllClientsEpic = action$ => {
-  return action$.pipe(
-    ofType(LOAD_ALL_CLIENTS),
-    switchMap((action: any) => {
-      return httpService.makeRequest('get', `${environment.API_URL}/client`, {}, true).pipe(
-        // If successful, dispatch success action with result
-        map(res => loadClientsCommit(res.data)),
-        // If request fails, dispatch failed action
-        catchError((error) => of(loadClientsFail(error)))
-      );
-    })
-  );
-};
+  /**
+   * this is here to handle asynchronous actions and will be triggered when LOAD_ONE_CLIENT action is dispatched
+   * @param {Observable} action$ - the current action
+   */
+  loadOneClientEpic = action$ => {
+    return action$.pipe(
+      reduxObservable.ofType(LOAD_ONE_CLIENT),
+      switchMap((action: Action) => {
+        return this.httpService.makeRequest('get', `${environment.API_URL}/client/${action.id}/`, {}, true).pipe(
+          // If successful, dispatch success action with result
+          map((res: Action) => loadOneClientCommit(res.data)),
+          // If request fails, dispatch failed action
+          catchError((error) => of(loadOneClientFail(error)))
+        );
+      })
+    );
+  };
 
-/**
- * this is here to handle asynchronous actions and will be triggered when LOAD_ONE_CLIENT action is dispatched
- * @param {Observable} action$ - the current action
- */
-const loadOneClientEpic = action$ => {
-  return action$.pipe(
-    reduxObservable.ofType(LOAD_ONE_CLIENT),
-    switchMap((action: Action) => {
-      return httpService.makeRequest('get', `${environment.API_URL}/client/${action.id}/`, {}, true).pipe(
-        // If successful, dispatch success action with result
-        map((res: Action) => loadOneClientCommit(res.data)),
-        // If request fails, dispatch failed action
-        catchError((error) => of(loadOneClientFail(error)))
-      );
-    })
-  );
-};
+  /**
+   * this is here to handle asynchronous actions and will be triggered when CREATE_CLIENT action is dispatched
+   * @param {Observable} action$ - the current action
+   */
+  createClientEpic = action$ => {
+    return action$.pipe(
+      reduxObservable.ofType(CREATE_CLIENT),
+      switchMap((action: Action) => {
+        return this.httpService.makeRequest('post', `${environment.API_URL}/client/`, action.data, true).pipe(
+          // If successful, dispatch success action with result
+          map((res: Action) => createClientCommit(res.data, action.index)),
+          // If request fails, dispatch failed action
+          catchError((error) => of(createClientFail(error)))
+        );
+      })
+    );
+  }
 
-/**
- * this is here to handle asynchronous actions and will be triggered when CREATE_CLIENT action is dispatched
- * @param {Observable} action$ - the current action
- */
-const createClientEpic = action$ => {
-  return action$.pipe(
-    reduxObservable.ofType(CREATE_CLIENT),
-    switchMap((action: Action) => {
-      return httpService.makeRequest('post', `${environment.API_URL}/client/`, action.data, true).pipe(
-        // If successful, dispatch success action with result
-        map((res: Action) => createClientCommit(res.data, action.index)),
-        // If request fails, dispatch failed action
-        catchError((error) => of(createClientFail(error)))
-      );
-    })
-  );
-};
+  /**
+   * this is here to handle asynchronous actions and will be triggered when UPDATE_CLIENT action is dispatched
+   * @param {Observable} action$ - the current action
+   */
+  updateClientEpic = action$ => {
+    return action$.pipe(
+      reduxObservable.ofType(UPDATE_CLIENT),
+      switchMap((action: Action) => {
+        const payload = {...action.data};
+        delete payload['id']; // remove id from payload because we already send it in the url
+        return this.httpService.makeRequest('patch', `${environment.API_URL}/client/${action.data.id}/`, payload, true).pipe(
+          // If successful, dispatch success action with result
+          map((res: Action) => updateClientCommit(res.data)),
+          // If request fails, dispatch failed action
+          catchError((error) => of(updateClientFail(error)))
+        );
+      })
+    );
+  }
 
-/**
- * this is here to handle asynchronous actions and will be triggered when UPDATE_CLIENT action is dispatched
- * @param {Observable} action$ - the current action
- */
-const updateClientEpic = action$ => {
-  return action$.pipe(
-    reduxObservable.ofType(UPDATE_CLIENT),
-    switchMap((action: Action) => {
-      const payload = {...action.data};
-      delete payload['id']; // remove id from payload because we already send it in the url
-      return httpService.makeRequest('patch', `${environment.API_URL}/client/${action.data.id}/`, payload, true).pipe(
-        // If successful, dispatch success action with result
-        map((res: Action) => updateClientCommit(res.data)),
-        // If request fails, dispatch failed action
-        catchError((error) => of(updateClientFail(error)))
-      );
-    })
-  );
-};
+  /**
+   * this is here to handle asynchronous actions and will be triggered when DELETE_CLIENT action is dispatched
+   * @param {Observable} action$ - the current action
+   */
+  deleteClientEpic = action$ => {
+    return action$.pipe(
+      reduxObservable.ofType(DELETE_CLIENT),
+      switchMap((action: Action) => {
+        return this.httpService.makeRequest('delete', `${environment.API_URL}/client/${action.data.id}/`, {},  true).pipe(
+          // If successful, dispatch success action with result
+          map(res => deleteClientCommit(action.data)),
+          // If request fails, dispatch failed action
+          catchError((error) => of(deleteClientFail(error)))
+        );
+      })
+    );
+  }
 
-/**
- * this is here to handle asynchronous actions and will be triggered when DELETE_CLIENT action is dispatched
- * @param {Observable} action$ - the current action
- */
-const deleteClientEpic = action$ => {
-  return action$.pipe(
-    reduxObservable.ofType(DELETE_CLIENT),
-    switchMap((action: Action) => {
-      return httpService.makeRequest('delete', `${environment.API_URL}/client/${action.data.id}/`, {},  true).pipe(
-        // If successful, dispatch success action with result
-        map(res => deleteClientCommit(action.data)),
-        // If request fails, dispatch failed action
-        catchError((error) => of(deleteClientFail(error)))
-      );
-    })
-  );
-};
-// combine the modules epics into one
-export const clientsEpics = reduxObservable.combineEpics(
-  loadAllClientsEpic,
-  loadOneClientEpic,
-  updateClientEpic,
-  deleteClientEpic,
-  createClientEpic,
-);
+  constructor(
+    private httpService: HttpService
+  ) {
+    return reduxObservable.combineEpics(
+      this.loadAllClientsEpic,
+      this.loadOneClientEpic,
+      this.updateClientEpic,
+      this.deleteClientEpic,
+      this.createClientEpic,
+    );
+  }
+}
