fcbe8ebae28f9a1ad417677da6fc099ff8af12bb
Aziz <aziz@humanitec.com>
2019-05-06 12:03:10 +0200

    fix error in module

--- src/lib/blueprint-client.module.ts
+++ src/lib/blueprint-client.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MidgardSharedTranslationModule } from '@midgard/modules/translation/translation.shared.module';
 import { BlueprintClientRoutingModule } from './blueprint-client-routing.module';
 import { BlueprintClientComponent } from './blueprint-client.component';
 
 @NgModule({
   imports: [
-    MidgardSharedTranslationModule,
     BlueprintClientRoutingModule,
   ],
   declarations: [BlueprintClientComponent],
