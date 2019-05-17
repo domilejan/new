6d07876f05ef86ad53552267ccc8cb72c74833f8
Aziz <aziz@humanitec.com>
2019-05-17 13:59:47 +0200

    fix test

--- src/lib/blueprint-client.component.spec.ts
+++ src/lib/blueprint-client.component.spec.ts
@@ -1,13 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import {ClientComponent} from './blueprint-client.component';
+import { BlueprintClientComponent } from './blueprint-client.component';
 import { StoreStub } from '../../../../midgard/testing-utilities/stubs';
 import { Store } from '../../../../midgard/modules/store/store';
 
 describe('ClientComponent', () => {
-  let component: ClientComponent;
-  let fixture: ComponentFixture<ClientComponent>;
+  let component: BlueprintClientComponent;
+  let fixture: ComponentFixture<BlueprintClientComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,14 +15,14 @@ describe('ClientComponent', () => {
       providers: [
         { provide: Store, useClass: StoreStub },
       ],
-      declarations: [ ClientComponent ],
+      declarations: [ BlueprintClientComponent ],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ClientComponent);
+    fixture = TestBed.createComponent(BlueprintClientComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
